10fe6e013e104d85ba2d05445cd02856d0b2689a
Antonio Trindade <atrindade@umov.me>
2024-11-07 21:41:54 -0300

    Adiciona webdriverwait

--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -2,6 +2,7 @@ package com.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
 import java.util.Random;
 
 import org.junit.jupiter.api.AfterEach;
@@ -11,30 +12,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CalculadoraTest {
 	
 	public WebDriver driver;
+	public Random random; 
+	//WebDriver wait sendo declarado
+	public WebDriverWait wait;
 
 	@BeforeEach
 	public void setUp() throws Exception {
 		System.setProperty("webdriver.chrome.driver", "/Users/umov.me/Dev/drivers/chromedriver");
 		driver = new ChromeDriver();
+		//Espera implícita
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//WebDriverWait sendo criado
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		random = new Random();
+		
 		driver.get("https://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
 		Thread.sleep(2000);
 	}
 
 	@AfterEach
-	public void tearDown() throws Exception {
-		Thread.sleep(2000);
+	public void tearDown() throws Exception {		
 		driver.quit();
 	}
 	
 	@Test
 	public void testSomaPositivos() throws InterruptedException {
-		
-		Random random = new Random();
-		
+
 		Integer number1 = random.nextInt(100);
 		Integer number2 = random.nextInt(100);
 		Integer valorTotal = 0;
@@ -52,8 +62,10 @@ public class CalculadoraTest {
 		//O teste calcula o valor
 		valorTotal = number1 + number2;
 		
-		Thread.sleep(3000);
-		
+		//utiliza espera explicita
+		wait.until(ExpectedConditions
+				.textToBePresentInElementValue(By.xpath("//input[@id='total']"), valorTotal.toString()));
+				
 		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));		
 	
 	}
