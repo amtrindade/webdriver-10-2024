c5ecc589e38957c681ac943687a49cde41518b72
Antonio Trindade <atrindade@umov.me>
2024-11-11 19:30:44 -0300

    Refactor calculadora

--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -21,6 +21,9 @@ public class CalculadoraTest {
 	public Random random; 
 	//WebDriver wait sendo declarado
 	public WebDriverWait wait;
+	private WebElement tfNumber1;
+	private WebElement tfNumber2;
+	private WebElement tfTotal;
 
 	@BeforeEach
 	public void setUp() throws Exception {
@@ -33,7 +36,14 @@ public class CalculadoraTest {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		random = new Random();
 		
+		//Abre a página
 		driver.get("https://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
+		
+		//mapeia
+		tfNumber1 = driver.findElement(By.id("number1"));
+		tfNumber2 = driver.findElement(By.id("number2"));
+		tfTotal = driver.findElement(By.xpath("//input[@id='total']"));
+		
 		Thread.sleep(2000);
 	}
 
@@ -48,13 +58,9 @@ public class CalculadoraTest {
 		Integer number1 = random.nextInt(100);
 		Integer number2 = random.nextInt(100);
 		Integer valorTotal = 0;
-		
-		WebElement tfNumber1 = driver.findElement(By.id("number1"));
-		WebElement tfNumber2 = driver.findElement(By.id("number2"));
+	
 		WebElement btnSomar = driver.findElement(By.id("somar"));	
-		WebElement tfTotal = driver.findElement(By.xpath("//input[@id='total']"));
-		
-				
+						
 		tfNumber1.sendKeys(number1.toString());
 		tfNumber2.sendKeys(number2.toString());
 		btnSomar.click();
@@ -69,5 +75,92 @@ public class CalculadoraTest {
 		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));		
 	
 	}
+	
+	@Test
+	public void testSubtracao() {
+		Integer number1 = random.nextInt(100);
+		Integer number2 = random.nextInt(100);
+		Integer valorTotal = 0;
+	
+		WebElement btnSubtrair = driver.findElement(By.id("subtrair"));	
+						
+		tfNumber1.sendKeys(number1.toString());
+		tfNumber2.sendKeys(number2.toString());
+		btnSubtrair.click();
+		
+		//O teste calcula o valor
+		valorTotal = number1 - number2;
+		
+		//utiliza espera explicita
+		wait.until(ExpectedConditions
+				.textToBePresentInElementValue(By.xpath("//input[@id='total']"), valorTotal.toString()));
+				
+		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));		
+	
+	}
+	
+	@Test
+	public void testMultiplicacao() {
+		Integer number1 = random.nextInt(100);
+		Integer number2 = random.nextInt(100);
+		Integer valorTotal = 0;
+	
+		WebElement btnMultiplica = driver.findElement(By.id("multiplicar"));	
+						
+		tfNumber1.sendKeys(number1.toString());
+		tfNumber2.sendKeys(number2.toString());
+		btnMultiplica.click();
+		
+		//O teste calcula o valor
+		valorTotal = number1 * number2;
+		
+		//utiliza espera explicita
+		wait.until(ExpectedConditions
+				.textToBePresentInElementValue(By.xpath("//input[@id='total']"), valorTotal.toString()));
+				
+		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));		
+	}
+	
+	@Test
+	public void testDivisao() {
+		Integer number1 = 100;
+		Integer number2 = 50;
+		Integer valorTotal = 0;
+	
+		WebElement btnDivisao = driver.findElement(By.id("dividir"));	
+						
+		tfNumber1.sendKeys(number1.toString());
+		tfNumber2.sendKeys(number2.toString());
+		btnDivisao.click();
+		
+		//O teste calcula o valor
+		valorTotal = number1 / number2;
+		
+		//utiliza espera explicita
+		wait.until(ExpectedConditions
+				.textToBePresentInElementValue(By.xpath("//input[@id='total']"), valorTotal.toString()));
+				
+		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));
+	}
+	
+	@Test
+	public void testDivisaoPorZero() {
+		Integer number1 = 100;
+		Integer number2 = 0;
+			
+		WebElement btnDivisao = driver.findElement(By.id("dividir"));	
+						
+		tfNumber1.sendKeys(number1.toString());
+		tfNumber2.sendKeys(number2.toString());
+		btnDivisao.click();
+				
+		//utiliza espera explicita
+		wait.until(ExpectedConditions
+				.textToBePresentInElementValue(By.xpath("//input[@id='total']"), "Infinity"));
+				
+		assertEquals("Infinity", tfTotal.getAttribute("value"));
+	}
+	
+	
 
 }
