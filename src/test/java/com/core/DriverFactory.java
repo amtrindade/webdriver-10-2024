cd38fd309e01ec08a799636db8ee6675fb9f451d
Antonio Trindade <atrindade@umov.me>
2024-11-11 21:10:02 -0300

    Adiciona DriverFactory

---
+++ src/test/java/com/core/DriverFactory.java
@@ -0,0 +1,32 @@
+package com.core;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+	
+	private static WebDriver driver;
+	
+	public static WebDriver getDriver() {
+		
+		if (driver == null) {			
+			System.setProperty("webdriver.chrome.driver", "/Users/umov.me/Dev/drivers/chromedriver");
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+		
+		return driver;		
+	}
+	
+	public static void killDriver() {
+		
+		if (driver != null) {
+			driver.quit();
+			driver = null;		
+		}		
+	}
+	
+
+}
